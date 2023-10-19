44656e232b350f947ac3300be672a79f18dd836e
Cody Hardy <chardy0919@gmail.com>
2023-10-19 16:28:15 -0400

    First Leetcode problems

---
+++ Search_Insert_Position.py
@@ -0,0 +1,17 @@
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left = nums[0]
+        right = len(nums)
+        while left < right:
+            middle = (right + left)//2
+            if target < nums[middle]:
+                right = middle
+            elif target > nums[middle]:
+                left = middle+1
+            else:
+                return middle
+        return left
